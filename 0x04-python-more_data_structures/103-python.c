3b66c864277129a6b25cd4cff469304823cd4bbd
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-07-27 13:00:58 +0100

    Fixed Output Error

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -25,7 +25,7 @@ void print_python_bytes(PyObject *p)
     else
         printf("  first 10 bytes:");
     for (i = 0; i < size && i < 10; i++)
-        printf(" %02x", trying_str[i]);
+        printf(" %02hhx", trying_str[i]);
     printf("\n");
 }
 
